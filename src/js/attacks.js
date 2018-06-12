8ceef9c85e0a0a477cdffaefaecb33149bd9f4ce
kriskuiper <kriskuiper74@gmail.com>
2018-06-12 21:40:25 +0200

    Inserted JS-lint comments in all files

--- src/js/attacks.js
+++ src/js/attacks.js
@@ -1,6 +1,7 @@
 /*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
 /*eslint-env browser*/
 /*eslint 'no-console':0*/
+
 // ====== start of script attacks =======
 
 // ====== aanvallen Student
